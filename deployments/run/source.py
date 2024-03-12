dfec1436138db277bd04119878d949deb1d52ccd
pugazh mukilan <pugazhmukilanoffical2004@gmail.com>
2024-03-12 03:19:12 +0000

    Deployed `run`

---
+++ deployments/run/source.py
@@ -0,0 +1,17 @@
+import modelbit, sys
+from typing import *
+from sklearn.linear_model._base import LinearRegression
+
+regressor = modelbit.load_value("data/regressor.pkl") # LinearRegression()
+
+# main function
+def run(num):
+    if num == None:
+        return None
+    else:
+        return regressor.predict([[num]])
+
+# to run locally via git & terminal, uncomment the following lines
+# if __name__ == "__main__":
+#   result = run(...)
+#   print(result)
