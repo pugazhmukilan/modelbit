390462858ef0658ee2ca4e44fac5af2783d8cd59
pugazh mukilan <pugazhmukilanoffical2004@gmail.com>
2024-03-13 03:30:39 +0000

    Deployed `My_First_deployment`

--- deployments/My_First_deployment/source.py
+++ deployments/My_First_deployment/source.py
@@ -5,8 +5,8 @@ from sklearn.linear_model._base import LinearRegression
 regressor = modelbit.load_value("data/regressor.pkl") # LinearRegression()
 
 # main function
-def first_regression():
-    answer  = regressor.predict([[6.5]])
+def first_regression(num):
+    answer  = regressor.predict([[num]])
     
     return answer
 
