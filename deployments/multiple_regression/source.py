7f3d85180a317cab4ef18941ea097739f0fe2542
pugazh mukilan <pugazhmukilanoffical2004@gmail.com>
2024-04-03 06:54:54 +0000

    Deployed `multiple_regression`

--- deployments/multiple_regression/source.py
+++ deployments/multiple_regression/source.py
@@ -5,11 +5,11 @@ from sklearn.linear_model._base import LinearRegression
 regressor = modelbit.load_value("data/regressor.pkl") # LinearRegression()
 
 # main function
-def run(num):
-    if num == None:
+def run(num1,num2,num3,num4,num5,num6):
+    if num1 == None:
         return None
     else:
-        return regressor.predict([num])
+        return regressor.predict([num1,num2,num3,num4,num5,num6])
 
 # to run locally via git & terminal, uncomment the following lines
 # if __name__ == "__main__":
